05b9239720384a178ec40d8616792354bbd137c4
Mrutunjay Panda <bapunpanda63@gmail.com>
2023-11-17 22:38:21 +0530

    Comitted

---
+++ src/Constants/Character_Data.js
@@ -0,0 +1,482 @@
+const Character_Data = [
+    {
+        charName : "Jean",
+        region : "MondStadt",
+        jpVoice : "SAITO Chiwa",
+        enVoice : "Stephanie Southerland",
+        desc : "As the Acting Grand Master of the Knights, Jean has always been devoted to her duties and maintaining peace in Mondstadt.She had taken precautions long before the onset of Stormterror's assault, and she will guard Mondstadt with her life as always.",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : 5,
+        element : "Anemo"
+    },
+    
+    {
+        charName : "Amber",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : 4,
+        element: "Pyro"
+    },
+    
+    {
+        charName : "Lisa",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : 4,
+        element : "Electro"
+    },
+    
+    {
+        charName : "Kaeya",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : 4,
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Barbara",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element:"Hydro"
+    },
+    
+    {
+        charName : "Diluc",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element:"Pyro"
+    },
+    
+    {
+        charName : "Razor",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Electro"
+    },
+    
+    {
+        charName : "Venti",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Anemo"
+    },
+    
+    {
+        charName : "Klee",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Pyro"
+    },
+    
+    {
+        charName : "Bennet",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Pyro"
+    },
+    
+    {
+        charName : "Noelle",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Geo"
+    },
+    
+    {
+        charName : "Fischl",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Electro"
+    },
+    
+    {
+        charName : "Sucrose",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Anemo"
+    },
+    
+    {
+        charName : "Mona",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Hydro"
+    },
+    
+    {
+        charName : "Diona",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Albedo",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Geo"
+    },
+    
+    {
+        charName : "Rosaria",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Eula",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Aloy",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Mika",
+        region : "MondStadt",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Cryo"
+    },
+
+    {
+        charName : "Xiao",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Anemo"
+    },
+    
+    {
+        charName : "Beidou",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Electro"
+    },
+    
+    {
+        charName : "Ninguang",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Geo"
+    },
+    
+    {
+        charName : "Xiangling",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Pyro"
+    },
+    
+    {
+        charName : "Xingqiu",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Hydro"
+    },
+    
+    {
+        charName : "Chongyun",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Qiqi",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Kequing",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Electro"
+    },
+    
+    {
+        charName : "Tartaglia",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Hydro"
+    },
+    
+    {
+        charName : "Zhongli",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Geo"
+    },
+    
+    {
+        charName : "Xinyan",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Pyro"
+    },
+    
+    {
+        charName : "Ganyu",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Hutao",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Pyro"
+    },
+    
+    {
+        charName : "Yanfei",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Pyro"
+    },
+    
+    {
+        charName : "Shenhe",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Cryo"
+    },
+    
+    {
+        charName : "Yun Jin",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Geo"
+    },
+    
+    {
+        charName : "Yelan",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Hydro"
+    },
+    
+    {
+        charName : "Yaoyao",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "4",
+        element : "Dendro"
+    },
+    
+    {
+        charName : "Baizhu",
+        region : "Liyue",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : "Dendro"
+    },
+    
+    {
+        charName : "",
+        region : "Inazuma",
+        jpVoice : "",
+        enVoice : "",
+        desc : "",
+        Char_Img : "",
+        Char_Profile : "",
+        stars : "5",
+        element : ""
+    },
+    
+]
